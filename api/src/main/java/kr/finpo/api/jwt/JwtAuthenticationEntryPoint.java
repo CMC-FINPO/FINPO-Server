29f221dc95249962bf3750a172bb9cd3c91707da
Mason <mskim9967@gmail.com>
2022-05-25 10:11:06 +0900

    Feat #1 - Access/Refresh token
    Spring Security 사용하여 access / refresh token 생성 및 반환, 검증

---
+++ api/src/main/java/kr/finpo/api/jwt/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,28 @@
+package kr.finpo.api.jwt;
+
+import kr.finpo.api.constant.ErrorCode;
+import net.minidev.json.JSONObject;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+  @Override
+  public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+    response.setHeader("content-type", "application/json");
+    response.setStatus(401);
+
+    ErrorCode errorcode = ErrorCode.ACCESS_TOKEN_EXPIRATION;
+    JSONObject params = new JSONObject();
+    params.put("success", false);
+    params.put("errorCode", errorcode.getCode().toString());
+    params.put("message", errorcode.getMessage());
+    response.getWriter().write(params.toJSONString());
+  }
+}
---
+++ api/src/main/java/kr/finpo/api/service/CustomUserDetailsService.java
@@ -0,0 +1,42 @@
+package kr.finpo.api.service;
+
+import kr.finpo.api.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collections;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+  @Autowired
+  private UserRepository userRepository;
+
+
+  @Override
+  @Transactional
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    return userRepository.findById(Long.parseLong(username))
+        .map(this::createUserDetails).get();
+  }
+
+
+  private UserDetails createUserDetails(kr.finpo.api.domain.User user) {
+    GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(user.getRole().toString());
+
+    return new User(
+        user.getId().toString(),
+        null,
+        Collections.singleton(grantedAuthority)
+    );
+  }
+}
