eee6c2467b5ab60e177da2534d4b8030da0eec46
Mason <mskim9967@gmail.com>
2022-05-25 19:27:14 +0900

    Fix - Kakao repository id type
    id type이 String인데 Long으로 잘못 작성해놓음

--- api/src/main/java/kr/finpo/api/repository/KakaoAccountRepository.java
+++ api/src/main/java/kr/finpo/api/repository/KakaoAccountRepository.java
@@ -1,12 +1,14 @@
 package kr.finpo.api.repository;
 
 import kr.finpo.api.domain.KakaoAccount;
+import kr.finpo.api.domain.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface KakaoAccountRepository extends JpaRepository<KakaoAccount, Long> {
+public interface KakaoAccountRepository extends JpaRepository<KakaoAccount, String> {
+
 }
 
