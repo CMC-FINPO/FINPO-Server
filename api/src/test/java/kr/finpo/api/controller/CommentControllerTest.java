82197771081372114de6729b4af51f6f09040029
Mason <mskim9967@gmail.com>
2022-07-15 19:33:02 +0900

    Docs

--- api/src/test/java/kr/finpo/api/controller/CommentControllerTest.java
+++ api/src/test/java/kr/finpo/api/controller/CommentControllerTest.java
@@ -1,7 +1,10 @@
 package kr.finpo.api.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import kr.finpo.api.constant.ErrorCode;
+import kr.finpo.api.dto.BlockedUserDto;
+import kr.finpo.api.dto.InterestRegionDto;
 import kr.finpo.api.repository.*;
 import net.minidev.json.JSONObject;
 import net.minidev.json.parser.JSONParser;
@@ -27,7 +30,9 @@ import org.springframework.transaction.annotation.Propagation;
 import javax.transaction.Transactional;
 import java.io.FileInputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 import static org.assertj.core.api.BDDAssertions.then;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
@@ -62,8 +67,7 @@ class CommentControllerTest {
   private BlockedUserRepository blockedUserRepository;
 
 
-
-  String accessToken, refreshToken, otherAccessToken, anotherAccessToken;
+  String accessToken, refreshToken, otherAccessToken, anotherAccessToken, aac;
 
   PostControllerTest pc = new PostControllerTest();
   UserControllerTest uc = new UserControllerTest();
@@ -89,6 +93,9 @@ class CommentControllerTest {
     map = oc.registerAndGetToken(mockMvc, "wwwwwlllll");
     anotherAccessToken = map.get("accessToken");
 
+    map = oc.registerAndGetToken(mockMvc, "차단할유저");
+    aac = map.get("accessToken");
+
     NotificationControllerTest nc = new NotificationControllerTest();
     nc.set(mockMvc, accessToken);
     nc.updateMy(accessToken);
@@ -103,9 +110,9 @@ class CommentControllerTest {
     insert(postId, "1빠당", anotherAccessToken, false);
     insert(postId, "2빠당", anotherAccessToken, false);
     insertAnonymity(postId, "3빠당", anotherAccessToken, false);
-    insertAnonymity(postId, "4빠당", anotherAccessToken, false);
+    int blockedId3 = insert(postId, "4빠당", aac, false);
     insert(postId, "5빠당", anotherAccessToken, false);
-    insert(postId, "6빠당", anotherAccessToken, false);
+    int blockId2 = insert(postId, "6빠당", otherAccessToken, false);
     int parentId = insertAnonymity(postId, "익명으로 다는 댓글임", otherAccessToken, false);
     insert(postId, "작성자가쓰는글", accessToken, false);
     int blockId = insertAnonymity(postId, "익명으로 단 사람이 또 단 댓글임", otherAccessToken, false);
@@ -119,10 +126,12 @@ class CommentControllerTest {
     update(updateId, "글작성자가 쓰는 세번째 대댓글 수정된거임", accessToken, false);
     uc.deleteMe(anotherAccessToken);
     blockUser(blockId, accessToken);
-    getByPostId(postId);
+    blockUser(blockId2, accessToken);
+    blockUser(blockedId3, accessToken);
+    getByPostId(postId, accessToken);
   }
 
-  void getByPostId(int postId) throws Exception {
+  void getByPostId(int postId, String accessToken) throws Exception {
     mockMvc.perform(RestDocumentationRequestBuilders.get("/post/{id}/comment?page=1&size=5&sort=id,asc", postId)
             .contentType(MediaType.APPLICATION_JSON)
             .header("Authorization", "Bearer " + accessToken)
@@ -401,7 +410,8 @@ class CommentControllerTest {
         .andExpect(jsonPath("$.success").value(true))
         .andExpect(jsonPath("$.errorCode").value(ErrorCode.OK.getCode()))
         .andDo(
-            !doDocument ? result -> {} : document("댓글수정",
+            !doDocument ? result -> {
+            } : document("댓글수정",
                 preprocessRequest(prettyPrint()),
                 preprocessResponse(prettyPrint()),
                 requestHeaders(
@@ -445,7 +455,7 @@ class CommentControllerTest {
   void report(int id, String accessToken, String documentName) throws Exception {
     HashMap<String, Object> body = new HashMap<>();
     ObjectMapper objectMapper = new ObjectMapper();
-    body.put("report", new HashMap<>(){{
+    body.put("report", new HashMap<>() {{
       put("id", "3");
     }});
 
@@ -518,7 +528,76 @@ class CommentControllerTest {
                     , fieldWithPath("data").description("성공 여부")
                 )
             )
+        ).andReturn();
+  }
+
+  @Test
+  List<BlockedUserDto> getMyBlock() throws Exception {
+    getByPostId();
+
+    MvcResult res = mockMvc.perform(get("/user/block/me")
+            .contentType(MediaType.APPLICATION_JSON)
+            .header("Authorization", "Bearer " + accessToken)
+        )
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$.success").value(true))
+        .andExpect(jsonPath("$.errorCode").value(ErrorCode.OK.getCode()))
+        .andDo(
+            document("차단한유저조회",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                requestHeaders(
+                    headerWithName("Authorization").description("Access Token")
+                ),
+                relaxedResponseFields(
+                    fieldWithPath("success").description("성공 여부"),
+                    fieldWithPath("errorCode").description("응답 코드"),
+                    fieldWithPath("message").description("응답 메시지")
+                    , fieldWithPath("data.[].id").description("차단 id")
+                    , fieldWithPath("data.[].createdAt").description("차단일")
+                    , fieldWithPath("data.[].blockedUser").description("차단된 유저")
+                )
+            )
+        ).andReturn();
+
+    JSONParser parser = new JSONParser();
+    JSONObject json = (JSONObject) parser.parse(res.getResponse().getContentAsString());
+    return Arrays.asList(new ObjectMapper().registerModule(new JavaTimeModule()).readValue(json.get("data").toString(), BlockedUserDto[].class));
+  }
+
+  @Test
+  void blockUserDeleteTest() throws Exception {
+    int postId = pc.insertPost(accessToken);
+    int commentId = insert(postId, "1빠당", anotherAccessToken, false);
+    blockUser(commentId, accessToken);
+    Long blockId = getMyBlock().get(0).id();
+    long beforeCnt = blockedUserRepository.count();
+
+    mockMvc.perform(RestDocumentationRequestBuilders.delete("/user/block/{id}", blockId)
+            .contentType(MediaType.APPLICATION_JSON)
+            .header("Authorization", "Bearer " + accessToken)
+        )
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$.success").value(true))
+        .andExpect(jsonPath("$.errorCode").value(ErrorCode.OK.getCode()))
+        .andDo(
+            document("차단한유저해제",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                requestHeaders(
+                    headerWithName("Authorization").description("Access Token")
+                ),
+                relaxedResponseFields(
+                    fieldWithPath("success").description("성공 여부"),
+                    fieldWithPath("errorCode").description("응답 코드"),
+                    fieldWithPath("message").description("응답 메시지")
+                    , fieldWithPath("data").description("차단 해제 여부")
+                )
+            )
         );
+    then(beforeCnt - 1).isEqualTo(blockedUserRepository.count());
   }
 
 }
