890d8abf970e466e40a1d1ec6201bb08316ff7ac
Mason Kim <mskim9967@gmail.com>
2022-07-22 23:09:24 +0900

    Fix - notification history bug
    Query 버그 해결

--- api/src/main/java/kr/finpo/api/repository/NotificationRepository.java
+++ api/src/main/java/kr/finpo/api/repository/NotificationRepository.java
@@ -9,10 +9,10 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
-  @Query(value = "SELECT n FROM Notification n WHERE n.user.id = :userId AND n.id < :lastId AND (n.comment IS NULL OR n.comment.status = true)")
+  @Query(value = "SELECT n FROM Notification n WHERE n.user.id = :userId AND n.id < :lastId AND (n.comment.status = true OR n.policy.status = true)")
   public Page<Notification> findByUserId(Long userId, Long lastId, Pageable pageable);
 
-  @Query(value = "SELECT n FROM Notification n WHERE n.user.id = :userId AND (n.comment IS NULL OR n.comment.status = true)")
+  @Query(value = "SELECT n FROM Notification n LEFT JOIN Comment c ON n.comment = c LEFT JOIN Policy p ON n.policy = p WHERE n.user.id = :userId AND (p.status = true OR c.status = true)")
   public Page<Notification> findByUserId(Long userId, Pageable pageable);
 
   public Long deleteByUserId(Long userId);
